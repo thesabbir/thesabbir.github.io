322014903a876dc96fd66c48819602dffbf8a03d
Sabbir Ahmed <sabbiirr@gmail.com>
2016-07-20 19:12:13 +0600

    redirector

---
+++ redirectorDB.js
@@ -0,0 +1,11 @@
+/**
+ * All links NEED http:// in-front to work
+ * Links with special chars need to be in single quotes
+ * redirectorDB is just a javascript object so js syntax applies but be consistent.
+ *
+ */
+
+var redirectorDB = {
+    "blog": "https://thesabbir.com"
+};
+
